b3fa8b19cde3f6abbf997db1570f09fb75773d63
tanhuang <421526639@qq.com>
2018-04-26 16:21:20 +0800

    修改后的提交

---
+++ THDatePickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  THDatePickerView
+//
+//  Created by 希达 on 2018/4/26.
+//  Copyright © 2018年 Tan.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ THDatePickerView/THDatePickerView.h
@@ -0,0 +1,48 @@
+//
+//  THDatePickerView.h
+//  rongyp-company
+//
+//  Created by Apple on 2016/11/16.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol THDatePickerViewDelegate <NSObject>
+
+/**
+ 保存按钮代理方法
+ 
+ @param timer 选择的数据
+ */
+- (void)datePickerViewSaveBtnClickDelegate:(NSString *)timer;
+
+/**
+ 取消按钮代理方法
+ */
+- (void)datePickerViewCancelBtnClickDelegate;
+
+@end
+
+@interface THDatePickerView : UIView
+
+@property (copy, nonatomic) NSString *title;
+
+/// 是否自动滑动 默认YES
+@property (assign, nonatomic) BOOL isSlide;
+
+/// 选中的时间， 默认是当前时间 2017-02-12 13:35
+@property (copy, nonatomic) NSString *date;
+
+/// 分钟间隔 默认5分钟
+@property (assign, nonatomic) NSInteger minuteInterval;
+
+@property (weak, nonatomic) id <THDatePickerViewDelegate> delegate;
+
+
+/**
+ 显示  必须调用
+ */
+- (void)show;
+
+@end
